d7480e55d15a61c99814a6bd67be1b4a3fab80bb
Eugenia <musat.eugenia@gmail.com>
2024-02-27 14:50:00 +0100

    Add message in program to the user

--- PesonalRegister/Program.cs
+++ PesonalRegister/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Välkommen till Eugenias Restaurang Personal Register");
+
+            Console.WriteLine("Var snäll och lägg till anställdas förnamn: ");
+            string förNamn = Console.ReadLine();
+
+            Console.WriteLine($"Var snäll och lägg till {förNamn} efternamn: ");
+            string efterNamn = Console.ReadLine();
+
+            Console.WriteLine($"Var snäll och lägg till {efterNamn} lön: ");
+            string lön = Console.ReadLine();
+
+
         }
     }
 }
