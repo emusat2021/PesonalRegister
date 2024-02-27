9dcde37c976f5c71aa8a4ccc0cc53d4a0d3f814e
Eugenia <musat.eugenia@gmail.com>
2024-02-27 16:42:05 +0100

    Test if the test folder is working

---
+++ PersonalRegister.tests/LäggatillAnställdaTests.cs
@@ -0,0 +1,14 @@
+using PersonalRegister;
+
+namespace PersonalRegister.tests;
+
+public class LäggatillAnställdaTests
+{
+    [Fact]
+    public void lägg__en_anställd_till_list()
+    {
+        var expected = "MARIO";
+        var actual = "MARIO";
+        Assert.Equal(expected, actual);
+    }
+}
